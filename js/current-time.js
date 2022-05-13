9de7f409cdbf8b4dae0aa38814e95fc7682c87b1
carlosfpj <cafer89@gmail.com>
2022-05-12 22:36:19 -0500

    js and calendar-day mockup

---
+++ js/current-time.js
@@ -0,0 +1,40 @@
+function setCurrentTimePosition () {
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay()
+  const currentHour = currentDate.getHours()
+  const currentMin = currentDate.getMinutes()
+
+  const $currentTime = document.querySelector('.currentTime')
+
+  const $calendar = document.querySelector('.calendar-week')
+  let calendarBlockSize = $calendar.clientHeight
+  let calendarInlineSize = $calendar.clientWidth
+
+  const calendarTimezoneCellInlineSize = document.querySelector('.timezoneCell').clientWidth
+  calendarInlineSize = calendarInlineSize - calendarTimezoneCellInlineSize
+
+  const cellInlineSize = calendarInlineSize / 7
+
+  const calendarDayBlockSize = document.querySelector('.calendarDay').clientHeight
+
+  calendarBlockSize = calendarBlockSize - calendarDayBlockSize
+
+  const cellBlockSize = calendarBlockSize / 24
+
+  $currentTime.style.top = `${cellBlockSize * (currentHour) + calendarDayBlockSize + (cellBlockSize / 60) * currentMin}px`
+  $currentTime.style.left = `${cellInlineSize * (currentDay) + calendarTimezoneCellInlineSize + 24}px`
+  console.log($currentTime.style.left);
+}
+
+window.intervalCurrentTimePosition = null
+
+function intervalCurrentTimePosition(interval = 1000) {
+  clearInterval(window.intervalCurrentTimeposition)
+  setCurrentTimePosition()
+  window.intervalCurrentTimeposition = setInterval(setCurrentTimePosition, interval)
+}
+
+export {
+  setCurrentTimePosition,
+  intervalCurrentTimePosition
+}
